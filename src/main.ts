042071f3378d2f49e87f5bf067fb8c236262ff51
saroj-rumsan <saroj.shrestha@rumsan.net>
2024-01-08 10:34:21 +0545

    feat: implement NestJS project with Fastify adapter. closes #1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { INestApplication, Logger, VersioningType } from '@nestjs/common';
+import CONSTANTS from 'src/utils/constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const _logger = new Logger(NestApplication?.name);
+	const app: INestApplication = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter({ logger: false }),
+		{
+			cors: true,
+		},
+	);
+	app.setGlobalPrefix('api').enableVersioning({ type: VersioningType?.URI, defaultVersion: '1' });
+	await app.listen(CONSTANTS?.port, 'localhost', () => {
+		_logger.log(`Application Listening in port ${CONSTANTS?.port}`);
+	});
 }
 bootstrap();
